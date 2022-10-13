5da371d5dd26f9b69536f4b603cf96fa4946aa11
Alex <alexwinston2123@icloud.com>
2022-10-13 13:46:51 -0500

    allPuppies component

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 const AllPuppies = (props) => {
+	const myPuppies = props.puppyData
+	console.log(myPuppies)
 	return ( 
-		<div className="box">("This is your AllPuppies component")</div>
 		
-	 );
-}
+		<div className="box">{"This is your AllPuppies component"}
+		{myPuppies.length ?
+			myPuppies.map((puppy) => {
+				console.log(puppy)
+			})
+	:	<div>Loading the Cutest Puppies...</div>}
+		</div>
+	)}
  
 export default AllPuppies;
