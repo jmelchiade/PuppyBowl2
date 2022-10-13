ec5be25af7385bdbe60df0c09c5a9fd9fc59e65a
Alex <alexwinston2123@icloud.com>
2022-10-13 14:06:53 -0500

    created allPuppies props, & key/ class for puppies variable

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -4,11 +4,18 @@ const AllPuppies = (props) => {
 	const myPuppies = props.puppyData
 	console.log(myPuppies)
 	return ( 
-		
-		<div className="box">{"This is your AllPuppies component"}
+		<div className='box'>{`This is your AllPuppies component`}
 		{myPuppies.length ?
 			myPuppies.map((puppy) => {
-				console.log(puppy)
+				return (
+					<SinglePuppy />
+				)
+					// <div key={`player-${puppy.id}`} className='puppy'>
+						{/* <div>{puppy.name}</div>
+						<div>{puppy.breed}</div>
+						<div>{puppy.status}</div> */}
+				// )
+				// {/* console.log(puppy) */}
 			})
 	:	<div>Loading the Cutest Puppies...</div>}
 		</div>
