732a779cae0328260b1c29a43af55873b2f46d2b
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-14 14:46:52 -0400

    updated imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-
 export { default as Main } from "./Main";
 export { default as Navbar } from "./Navbar";
 export { default as AllPuppies } from "./AllPuppies";
 export { default as SinglePuppy } from "./SinglePuppy";
+export { default as PuppyDetails } from "./PuppyDetails";
