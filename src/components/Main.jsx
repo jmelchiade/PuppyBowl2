e833dec1910e51f38e395f7fde25f861ebe7445e
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-14 21:42:31 -0400

    Revised uneeded text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, AllPuppies, SinglePuppy, PuppyDetails } from "./";
-import image from "./background.jpeg";
+
 
 
 const Main = () => {
